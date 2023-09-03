2f9521cfda21c751e6cf46050c1ec2f254599293
Mahdi Hasanzadeh <Mahdi.h2619@gmail.com>
2023-09-03 17:34:21 +0430

    Drawing Magnitude added

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -40,6 +40,11 @@ namespace DrawingForm
             this.txtX2 = new System.Windows.Forms.TextBox();
             this.btnDrawLine = new System.Windows.Forms.Button();
             this.btnDeleteLine = new System.Windows.Forms.Button();
+            this.txtMagnitude = new System.Windows.Forms.TextBox();
+            this.txtMagnitude2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnDrawDiagram = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // drawingPanel
@@ -50,27 +55,27 @@ namespace DrawingForm
             this.drawingPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.drawingPanel.Location = new System.Drawing.Point(12, 59);
             this.drawingPanel.Name = "drawingPanel";
-            this.drawingPanel.Size = new System.Drawing.Size(849, 467);
+            this.drawingPanel.Size = new System.Drawing.Size(841, 467);
             this.drawingPanel.TabIndex = 0;
             // 
             // txtX1
             // 
-            this.txtX1.Location = new System.Drawing.Point(362, 7);
+            this.txtX1.Location = new System.Drawing.Point(434, 7);
             this.txtX1.Name = "txtX1";
             this.txtX1.Size = new System.Drawing.Size(79, 20);
-            this.txtX1.TabIndex = 3;
+            this.txtX1.TabIndex = 0;
             // 
             // txtY1
             // 
-            this.txtY1.Location = new System.Drawing.Point(362, 33);
+            this.txtY1.Location = new System.Drawing.Point(434, 33);
             this.txtY1.Name = "txtY1";
             this.txtY1.Size = new System.Drawing.Size(79, 20);
-            this.txtY1.TabIndex = 4;
+            this.txtY1.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(338, 10);
+            this.label1.Location = new System.Drawing.Point(410, 10);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(18, 13);
             this.label1.TabIndex = 7;
@@ -79,7 +84,7 @@ namespace DrawingForm
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(338, 36);
+            this.label2.Location = new System.Drawing.Point(410, 36);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(18, 13);
             this.label2.TabIndex = 8;
@@ -88,7 +93,7 @@ namespace DrawingForm
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(472, 36);
+            this.label3.Location = new System.Drawing.Point(616, 33);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(18, 13);
             this.label3.TabIndex = 12;
@@ -97,7 +102,7 @@ namespace DrawingForm
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(472, 10);
+            this.label4.Location = new System.Drawing.Point(616, 7);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(18, 13);
             this.label4.TabIndex = 11;
@@ -105,24 +110,25 @@ namespace DrawingForm
             // 
             // txtY2
             // 
-            this.txtY2.Location = new System.Drawing.Point(496, 33);
+            this.txtY2.Location = new System.Drawing.Point(640, 30);
             this.txtY2.Name = "txtY2";
             this.txtY2.Size = new System.Drawing.Size(79, 20);
             this.txtY2.TabIndex = 10;
             // 
             // txtX2
             // 
-            this.txtX2.Location = new System.Drawing.Point(496, 7);
+            this.txtX2.Location = new System.Drawing.Point(640, 4);
             this.txtX2.Name = "txtX2";
             this.txtX2.Size = new System.Drawing.Size(79, 20);
             this.txtX2.TabIndex = 9;
             // 
             // btnDrawLine
             // 
+            this.btnDrawLine.AccessibleName = "";
             this.btnDrawLine.Location = new System.Drawing.Point(52, 12);
             this.btnDrawLine.Name = "btnDrawLine";
             this.btnDrawLine.Size = new System.Drawing.Size(113, 41);
-            this.btnDrawLine.TabIndex = 13;
+            this.btnDrawLine.TabIndex = 3;
             this.btnDrawLine.Text = "Draw Line";
             this.btnDrawLine.UseVisualStyleBackColor = true;
             // 
@@ -135,11 +141,57 @@ namespace DrawingForm
             this.btnDeleteLine.Text = "Delete Line";
             this.btnDeleteLine.UseVisualStyleBackColor = true;
             // 
+            // txtMagnitude
+            // 
+            this.txtMagnitude.Location = new System.Drawing.Point(519, 23);
+            this.txtMagnitude.Name = "txtMagnitude";
+            this.txtMagnitude.Size = new System.Drawing.Size(91, 20);
+            this.txtMagnitude.TabIndex = 2;
+            // 
+            // txtMagnitude2
+            // 
+            this.txtMagnitude2.Location = new System.Drawing.Point(725, 20);
+            this.txtMagnitude2.Name = "txtMagnitude2";
+            this.txtMagnitude2.Size = new System.Drawing.Size(119, 20);
+            this.txtMagnitude2.TabIndex = 16;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(532, 5);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Magnitude";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(762, 7);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 13);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Magnitude";
+            // 
+            // btnDrawDiagram
+            // 
+            this.btnDrawDiagram.Location = new System.Drawing.Point(290, 12);
+            this.btnDrawDiagram.Name = "btnDrawDiagram";
+            this.btnDrawDiagram.Size = new System.Drawing.Size(114, 41);
+            this.btnDrawDiagram.TabIndex = 19;
+            this.btnDrawDiagram.Text = "Draw Diagram";
+            this.btnDrawDiagram.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(873, 538);
+            this.ClientSize = new System.Drawing.Size(865, 538);
+            this.Controls.Add(this.btnDrawDiagram);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtMagnitude2);
+            this.Controls.Add(this.txtMagnitude);
             this.Controls.Add(this.btnDeleteLine);
             this.Controls.Add(this.btnDrawLine);
             this.Controls.Add(this.label3);
@@ -151,9 +203,8 @@ namespace DrawingForm
             this.Controls.Add(this.txtY1);
             this.Controls.Add(this.txtX1);
             this.Controls.Add(this.drawingPanel);
-            this.MaximumSize = new System.Drawing.Size(889, 577);
-            this.MinimumSize = new System.Drawing.Size(889, 577);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -173,6 +224,11 @@ namespace DrawingForm
         private System.Windows.Forms.TextBox txtX2;
         private System.Windows.Forms.Button btnDrawLine;
         private System.Windows.Forms.Button btnDeleteLine;
+        private System.Windows.Forms.TextBox txtMagnitude;
+        private System.Windows.Forms.TextBox txtMagnitude2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnDrawDiagram;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,7 +19,10 @@ namespace DrawingForm
         {
             InitializeComponent();
 
-            subscribeToEvents();
+            SubscribeToEvents();
+            _listOfPoints = new List<PointF>();
+            _listOfPointsForMagnitude = new List<PointF>();
+            _listOfCoordinatesForFillBrush = new List<PointF>();
         }
 
         #endregion
@@ -28,22 +32,23 @@ namespace DrawingForm
         private Graphics _panelGraphic;
         const int _gridSize = 100; // Adjust this value for the grid spacing
 
+        List<PointF> _listOfPoints;
+
+        List<PointF> _listOfPointsForMagnitude;
+
+        List<PointF> _listOfCoordinatesForFillBrush;
+
         #endregion
 
-        private void subscribeToEvents()
-        {
-            btnDrawLine.Click += BtnDrawLine_Click;
-            drawingPanel.Resize += DrawingPanel_Resize;
-            drawingPanel.Paint += DrawingPanel_Paint;
-            btnDeleteLine.Click += BtnDeleteLines_Click;
-            //This is for Git Check
-        }
-
+        #region Events
         private void BtnDeleteLines_Click(object sender, EventArgs e)
         {
             using (_panelGraphic = drawingPanel.CreateGraphics())
             {
                 _panelGraphic.Clear(Color.White);
+
+                _listOfPoints.Clear();
+                _listOfPointsForMagnitude.Clear();
 
                 DrawGrid();
             }
@@ -51,19 +56,20 @@ namespace DrawingForm
 
         private void DrawingPanel_Paint(object sender, PaintEventArgs e)
         {
+
             DrawGrid();
         }
 
-        private void DrawingPanel_Resize(object sender, EventArgs e)
-        {
-            // Get the Graphics object from the PictureBox
-            using (_panelGraphic = drawingPanel.CreateGraphics())
-            {
-                _panelGraphic.Clear(Color.White);
+        //private void DrawingPanel_Resize(object sender, EventArgs e)
+        //{
+        //    // Get the Graphics object from the PictureBox
+        //    using (_panelGraphic = drawingPanel.CreateGraphics())
+        //    {
+        //        _panelGraphic.Clear(Color.White);
 
-                //DrawGrid();
-            }
-        }
+        //        //DrawGrid();
+        //    }
+        //}
 
         private void BtnDrawLine_Click(object sender, EventArgs e)
         {
@@ -71,43 +77,228 @@ namespace DrawingForm
             float.TryParse(txtY1.Text, out float y1);
             float.TryParse(txtX2.Text, out float x2);
             float.TryParse(txtY2.Text, out float y2);
+
             DrawLine(x1, y1, x2, y2);
+
+            float.TryParse(txtMagnitude.Text, out float magnitude1);
+            float.TryParse(txtMagnitude2.Text, out float magnitude2);
+
+            //DrawMagnitude(x1, y1, magnitude1);
+
+            //if (_listOfPointsForMagnitude.Count >= 2)
+            //{
+            //    ConnectMagnitudes(_listOfPointsForMagnitude[0].X, _listOfPointsForMagnitude[0].Y, _listOfPointsForMagnitude[1].X, _listOfPointsForMagnitude[1].Y);
+
+            //    _listOfPointsForMagnitude.RemoveAt(0);
+            //}
+
+            //DrawMagnitude(x2, y2, magnitude2);
         }
+
+        private void ConnectMagnitudes(float x1, float y1, float x2, float y2)
+        {
+            using (_panelGraphic = drawingPanel.CreateGraphics())
+            {
+                Pen gridPen = new Pen(Color.Red, 3);
+
+                //var finalX1 = x1 + _gridSize;
+                //var finalY1 = drawingPanel.Height - y1 - _gridSize;
+                //var finalX2 = x2 + _gridSize;
+                //var finalY2 = drawingPanel.Height - y2 - _gridSize;
+
+                _panelGraphic.DrawLine(gridPen, x1, y1, x2, y2);
+
+                gridPen.Dispose();
+            }
+        }
+
+        #endregion
+
+
+        #region Private Methods
+
+        private void SubscribeToEvents()
+        {
+            btnDrawLine.Click += BtnDrawLine_Click;
+            //drawingPanel.Resize += DrawingPanel_Resize;
+            drawingPanel.Paint += DrawingPanel_Paint;
+            btnDeleteLine.Click += BtnDeleteLines_Click;
+            btnDrawDiagram.Click += BtnDrawDiagram_Click;
+            //This is for Git Check
+        }
+
+        private void BtnDrawDiagram_Click(object sender, EventArgs e)
+        {
+            float.TryParse(txtX1.Text, out float x1);
+            float.TryParse(txtY1.Text, out float y1);
+            float.TryParse(txtX2.Text, out float x2);
+            float.TryParse(txtY2.Text, out float y2);
+
+            float.TryParse(txtMagnitude.Text, out float magnitude1);
+            float.TryParse(txtMagnitude2.Text, out float magnitude2);
+
+            DrawMagnitude(x1, y1, magnitude1, x2, y2, magnitude2);
+
+
+        }
+
+        private float ConvertCoordinate(float value, bool isY = false)
+        {
+            return isY ? value - _gridSize : value + _gridSize;
+        }
+
+        private void DrawMagnitude(float x1, float y1, float magnitude1, float x2, float y2, float magnitude2)
+        {
+            x1 = ConvertCoordinate(x1);
+            x2 = ConvertCoordinate(x2);
+            y1 = ConvertCoordinate(y1,true);
+            y2 = ConvertCoordinate(y2,true);
+
+            var maxMagnitude = Math.Max(magnitude1, magnitude2);
+            var minMagnitude = Math.Min(magnitude1, magnitude2);
+
+            float length = (float) Math.Sqrt(Math.Pow(y2 - y1, 2) + Math.Pow(x2 - x1, 2));
+
+            var maxLength = length * 0.2f;
+
+
+            using (_panelGraphic = drawingPanel.CreateGraphics())
+            {
+                Pen gridPen = new Pen(Color.Black, 3);
+                var finalX1 = x1 ;
+                var finalY1 = drawingPanel.Height - y1;
+                var finalX2 = x1;
+                var finalY2 = drawingPanel.Height - y1  - maxLength;
+
+                //var finalMagnitude = (magnitude * 25) / 100;
+
+
+                _panelGraphic.DrawLine(gridPen, finalX1, finalY1, finalX2, finalY2);
+                _listOfPointsForMagnitude.Add(new PointF(finalX2, finalY2));
+                gridPen.Dispose();
+            }
+
+        }
+
+        private void DrawMagnitude(float x1, float y1, float magnitude)
+        {
+
+        }
+
 
         private void DrawLine(float x1, float y1, float x2, float y2)
         {
             using (_panelGraphic = drawingPanel.CreateGraphics())
             {
-                Pen gridPen = new Pen(Color.Blue, 4);
-                var finalx1 = x1 + _gridSize;
-                var finaly1 = drawingPanel.Height - y1 - _gridSize;
-                var finalx2 = x2 + _gridSize;
-                var finaly2 = drawingPanel.Height - y2 - _gridSize;
+                Pen gridPen = new Pen(Color.Blue, 3);
 
-                _panelGraphic.DrawLine(gridPen, finalx1, finaly1, finalx2, finaly2);
+                var finalX1 = x1 + _gridSize;
+                var finalY1 = drawingPanel.Height - y1 - _gridSize;
+                var finalX2 = x2 + _gridSize;
+                var finalY2 = drawingPanel.Height - y2 - _gridSize;
+                if (_listOfPoints.Count == 0)
+                {
+                    _panelGraphic.DrawLine(gridPen, finalX1, finalY1, finalX2, finalY2);
+                }
+                else
+                {
+                    //var count = _listOfPoints.Count;
+                    //PointF Point = _listOfPoints[_listOfPoints.Last().];
+                    finalX1 = _listOfPoints.Last().X + _gridSize;
+                    finalY1 = drawingPanel.Height - _listOfPoints.Last().Y - _gridSize;
+                    finalX2 = x1 + _gridSize;
+                    finalY2 = drawingPanel.Height - y1 - _gridSize;
+
+                    _panelGraphic.DrawLine(gridPen, finalX1, finalY1, finalX2, finalY2);
+                    //_listOfCoordinatesForFillBrush.Add()
+                }
+
+
+                //DrawCircle(finalX1, finalY1);
+                //DrawCircle(finalX2, finalY2);
+
+                _listOfPoints.Add(new PointF(x1, y1));
+
+
+                ClearInputs();
+
+                gridPen.Dispose();
+
+
+
 
 
                 // Define the points for the polygon
-                Point[] polygonPoints = new Point[]
-                {
-            new Point(100, 100),
-            new Point(200, 50),
-            new Point(300, 100),
-            new Point(250, 200),
-            new Point(150, 200)
-                };
+                //    Point[] polygonPoints = new Point[]
+                //    {
+                //new Point(100, 100),
+                //new Point(200, 50),
+                //new Point(300, 100),
+                //new Point(250, 200),
+                //new Point(150, 200)
+                //    };
 
-                // Create a Pen for the polygon outline
-                Pen outlinePen = new Pen(Color.Blue, 2);
-                Brush fillBrush = new SolidBrush(Color.Blue);
+                //    // Create a Pen for the polygon outline
+                //    Pen outlinePen = new Pen(Color.Blue, 2);
+                //    Brush fillBrush = new SolidBrush(Color.Blue);
 
                 // Draw the polygon
-                _panelGraphic.FillPolygon(fillBrush, polygonPoints);
+                //_panelGraphic.FillPolygon(fillBrush, polygonPoints);
 
-                // Dispose of the Pen object to release resources
-                outlinePen.Dispose();
+                //// Dispose of the Pen object to release resources
+                //outlinePen.Dispose();
             }
         }
+
+        private void ClearInputs()
+        {
+            //txtX1.Clear();
+            //txtX2.Clear();
+            //txtMagnitude.Clear();
+            //txtY1.Text = "";
+            //txtY2.Text = "";
+        }
+
+        private void DrawCircle(float x, float y)
+        {
+            using (_panelGraphic = drawingPanel.CreateGraphics())
+            {
+
+                int diameter = 4; // Diameter of the smaller rounded circle
+                int cornerRadius = 2; // Radius of the rounded corners
+
+                // Create a Brush for filling the smaller rounded circle (e.g., solid green)
+                Brush fillBrush = new SolidBrush(Color.Black);
+
+                // Create a rounded rectangle for the smaller rounded circle
+                RectangleF roundedRectangle = new RectangleF(x - 3, y - 3, diameter, diameter);
+                GraphicsPath roundedPath = GetRoundedRectangle(roundedRectangle, cornerRadius);
+
+                // Fill the smaller rounded circle
+                _panelGraphic.FillPath(fillBrush, roundedPath);
+
+                // Dispose of the Brush and GraphicsPath objects to release resources
+                fillBrush.Dispose();
+                roundedPath.Dispose();
+
+
+            }
+
+        }
+
+        // Helper method to create a rounded rectangle
+        private GraphicsPath GetRoundedRectangle(RectangleF rect, float radius)
+        {
+            GraphicsPath path = new GraphicsPath();
+            path.AddArc(rect.X, rect.Y, radius * 2, radius * 2, 180, 90); // Top-left corner
+            path.AddArc(rect.Right - 2 * radius, rect.Y, radius * 2, radius * 2, 270, 90); // Top-right corner
+            path.AddArc(rect.Right - 2 * radius, rect.Bottom - 2 * radius, radius * 2, radius * 2, 0, 90); // Bottom-right corner
+            path.AddArc(rect.X, rect.Bottom - 2 * radius, radius * 2, radius * 2, 90, 90); // Bottom-left corner
+            path.CloseFigure(); // Close the path
+
+            return path;
+        }
+
 
         private void DrawGrid()
         {
@@ -132,5 +323,6 @@ namespace DrawingForm
                 gridPen.Dispose();
             }
         }
+        #endregion
     }
 }
